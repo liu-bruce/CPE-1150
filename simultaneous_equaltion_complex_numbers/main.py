fc497925e73b25119eefb9bb9d983588df7fdaae
liu-bruce <87771104+liu-bruce@users.noreply.github.com>
2022-01-12 04:01:03 -0700

    Add files via upload

---
+++ simultaneous_equaltion_complex_numbers/main.py
@@ -0,0 +1,12 @@
+from simultaneous_equations_complex_numbers import equation_solve
+
+def main():
+    eq = equation_solve()
+    eq.compute()
+    eq.input_verification()
+
+
+
+
+if __name__ =="__main__":
+    main()
---
+++ simultaneous_equaltion_complex_numbers/simultaneous_equations_complex_numbers.py
@@ -0,0 +1,17 @@
+import numpy as eq
+
+#verifided works with j
+class equation_solve():
+    def __init__(self):
+        self.coeffecant_variables = [[630,-330,-300],[-330,-1690,-1000],[-300,-1000,1690]]
+        self.solutions_array = [12,0,0]
+        self.coeffecant_results = []
+
+    def input_verification(self):
+        print(self.coeffecant_results)
+        print("\n\n")
+        print(self.solutions_array)
+        print("\n\n")
+        print(self.coeffecant_results)
+    def compute(self):
+        self.coeffecant_results = eq.linalg.solve(self.coeffecant_variables,self.solutions_array)
